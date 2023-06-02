cd7bde920415dbc49b6a67229c4c3342e5425608
DeciBelioS <96150975+Deci8BelioS@users.noreply.github.com>
2023-06-02 03:29:31 +0200

    Create verificar-filtros.js

---
+++ verificar-filtros.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+const readFile = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, 'utf8', (error, data) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+const writeFile = (filename, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filename, data, 'utf8', (error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+const removeDuplicateFilters = (filters) => {
+  const uniqueFilters = new Map();
+  const filterRegex = /^([^!].*)$/gm;
+  let match;
+  while ((match = filterRegex.exec(filters)) !== null) {
+    const filter = match[1].trim();
+    if (!uniqueFilters.has(filter)) {
+      uniqueFilters.set(filter, true);
+    }
+  }
+  return Array.from(uniqueFilters.keys()).join('\n');
+};
+
+(async () => {
+  try {
+    const filters = await readFile('filtros.txt');
+    const uniqueFilters = removeDuplicateFilters(filters);
+    await writeFile('filtros-unicos.txt', uniqueFilters);
+    console.log('Filtros únicos generados en filtros-unicos.txt');
+  } catch (error) {
+    console.error('Error al procesar los filtros:', error);
+    process.exit(1);
+  }
+})();
