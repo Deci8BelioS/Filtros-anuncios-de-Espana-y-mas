84b9d5c1c5b2bdf335beb69ed6ce354cfe13ede7
DeciBelioS <96150975+Deci8BelioS@users.noreply.github.com>
2023-06-02 04:31:30 +0200

    Update verificar-filtros.js

--- verificar-filtros.js
+++ verificar-filtros.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const readFile = (filename) => {
   return new Promise((resolve, reject) => {
-    fs.readFile(filename, 'utf8', (error, data) => {
+    fs.readFile(filename, "utf8", (error, data) => {
       if (error) {
         reject(error);
       } else {
@@ -14,7 +14,7 @@ const readFile = (filename) => {
 
 const writeFile = (filename, data) => {
   return new Promise((resolve, reject) => {
-    fs.writeFile(filename, data, 'utf8', (error) => {
+    fs.writeFile(filename, data, "utf8", (error) => {
       if (error) {
         reject(error);
       } else {
@@ -26,29 +26,40 @@ const writeFile = (filename, data) => {
 
 const removeDuplicateFilters = (filters) => {
   const uniqueFilters = [];
-  const filterRegex = /^([^!].*)$/gm;
+  const filterRegex = /^(.*)$/gm; // Cambiado para incluir todas las líneas
+  const sectionRegex = /^!.*$/gm;
 
   let match;
+  let section = "";
 
   while ((match = filterRegex.exec(filters)) !== null) {
-    const filter = match[1].trim();
+    const line = match[0].trim();
 
-    if (!uniqueFilters.includes(filter)) {
-      uniqueFilters.push(filter);
+    if (section !== "") {
+      uniqueFilters.push(section);
+      section = "";
+    }
+
+    if (!uniqueFilters.includes(line)) {
+      uniqueFilters.push(line);
     }
   }
 
-  return uniqueFilters.join('\n');
+  if (section !== "") {
+    uniqueFilters.push(section);
+  }
+
+  return uniqueFilters.join("\n");
 };
 
 (async () => {
   try {
-    const filters = await readFile('filtro.txt');
+    const filters = await readFile("filtro.txt");
     const uniqueFilters = removeDuplicateFilters(filters);
-    const date = new Date().toISOString().split('T')[0];
-    const filterCount = uniqueFilters.split('\n').length;
+    const date = new Date().toISOString().split("T")[0];
+    const filterCount = uniqueFilters.split("\n").length;
     const header = [
-      '!',
+      "!",
       `! Homepage: https://github.com/Deci8BelioS/Filtros-anuncios-de-Espana-y-mas`,
       `! Title: Filtros anuncios de España y mas por DeciBelioS`,
       `! Description: Filtros de publicidad y elementos molestos para las extensiones uBlock Origin y AdGuard para paginas Españolas y mas (ejemplo: genbeta, xataka, motorpasion, reddit etc...) por DeciBelioS`,
@@ -57,14 +68,14 @@ const removeDuplicateFilters = (filters) => {
       `! License GPL-3.0: https://github.com/Deci8BelioS/Filtros-anuncios-de-Espana-y-mas/blob/main/LICENSE.md`,
       `! License CC-BY-3.0: https://github.com/Deci8BelioS/Filtros-anuncios-de-Espana-y-mas/blob/main/LICENSE-.md`,
       `!------ [Reglas genericas de bloqueo de publicidad] / general blocking ------!`,
-      '!',
-    ].join('\n');
+      "!",
+    ].join("\n");
 
     const finalContent = `${header}\n\n${uniqueFilters}`;
-    await writeFile('filtros-unicos.txt', finalContent);
-    console.log('Filtros únicos generados en filtros-unicos.txt');
+    await writeFile("filtros-unicos.txt", finalContent);
+    console.log("Filtros únicos generados en filtros-unicos.txt");
   } catch (error) {
-    console.error('Error al procesar los filtros:', error);
+    console.error("Error al procesar los filtros:", error);
     process.exit(1);
   }
 })();
